f0f00cf0e132c8daf70cbf5d0e949780de6232ec
laxman-136 <laxmansubramanyam@gmail.com>
2023-05-30 23:39:55 +0530

    web site commit

---
+++ popup.js
@@ -0,0 +1,28 @@
+
+
+let wrapper = document.getElementById('wrapper');
+let formButton = document.getElementById('submit');
+let Form = document.getElementById('contact-form');
+let popup = document.getElementById('popUpContainer');
+let closePopUp = document.getElementById('closeThePopUp');
+
+formButton.addEventListener('click', function (event) {
+  if (Form.checkValidity()) {
+
+    setTimeout(function () {
+      wrapper.style.filter = 'blur(8px)';
+      popup.style.display = 'block';
+    }, 2500); // Delay of 2.5 seconds (2500 milliseconds)
+  } else {
+    event.preventDefault(); // Prevent form submission if it's not valid
+  }
+});
+
+closePopUp.addEventListener('click', function () {
+  wrapper.style.filter = 'blur(0px)';
+  popup.style.display = 'none';
+});
+
+
+
+
